ecd4e3c1281fec5af89a4cbf9d4d483bd153e27f
noddy99 <nrodda993@gmail.com>
2022-02-06 09:23:02 +1100

    still function callback issue with asyncio

--- rpilcdmenu/rpi_lcd_hwd.py
+++ rpilcdmenu/rpi_lcd_hwd.py
@@ -92,7 +92,7 @@ class RpiLCDHwd:
 
     async def write4bits(self, bits, char_mode=False):
         """ Send command to LCD """
-        await self.delayMicroseconds(1000)  # 1000 microsecond sleep
+        await self.delayMicroseconds(1)  # 1000 microsecond sleep
         bits = bin(bits)[2:].zfill(8)
         await self.GPIO.write(self.pin_rs, char_mode)
         for pin in self.pins_db:
